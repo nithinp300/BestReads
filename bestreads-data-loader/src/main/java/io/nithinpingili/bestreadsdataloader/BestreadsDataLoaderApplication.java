5dd08f9370549cb65c210063e8e90b9f7a289587
Nithin Pingili <nithinp300@gmail.com>
2023-01-03 20:31:31 -0600

    created data loader spring boot app

---
+++ bestreads-data-loader/src/main/java/io/nithinpingili/bestreadsdataloader/BestreadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package io.nithinpingili.bestreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestreadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ bestreads-data-loader/src/test/java/io/nithinpingili/bestreadsdataloader/BestreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nithinpingili.bestreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
